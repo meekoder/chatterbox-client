a3c3147be1f0b07972e1a32fa7ea37c38927bacc
erenvilla <ecv.eren@gmail.com>
2020-10-17 13:05:56 -0400

    added functionality to initialize, render, and renderMessage methods

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,12 +3,21 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+    App.fetch((results) => {
+      this.render(results);
+    });
   },
 
-  render: function() {
+  render: function(messages) {
+    messages.forEach(message => {
+      this.renderMessage(message);
+    });
   },
 
   renderMessage: function(message) {
+    if (!message.username) {
+      return;
+    };
     // render new message
     var newMessage = MessageView.render(message);
     // append new message to body
